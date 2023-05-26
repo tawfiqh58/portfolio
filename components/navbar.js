979d1904980da1b91750a19e3739ed20480e2b2d
tawfiq58 <tawfiqhasan58@gmail.com>
2023-05-26 11:12:10 +0600

    doc: update

--- components/navbar.js
+++ components/navbar.js
@@ -30,17 +30,6 @@ export default function Navbar(props) {
     {
       label: "Archive",
       href: "/archive"
-    },
-    {
-      label: "Pro Version",
-      href: "https://stablo-pro.web3templates.com/",
-      external: true,
-      badge: "new"
-    },
-    {
-      label: "Download",
-      href: "https://web3templates.com/templates/stablo-minimal-blog-website-template",
-      external: true
     }
   ];
 
