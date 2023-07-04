bb33c9ccfeb3545a6bd28a65ea8544fb9aaa1529
Tawfiq Hasan <tawfiqhasan58@gmail.com>
2023-07-04 15:12:23 +0600

    fix: NEXTJS BUILD TIME STATIC PAGE URL FETCH

--- app/(website)/page.js
+++ app/(website)/page.js
@@ -1,7 +1,7 @@
 import HomePage from "./home";
-// import { getPorojects } from "@/lib/data";
+import { getPorojects } from "@/lib/data";
 
 export default async function IndexPage() {
-  // const posts = await getPorojects();
+  const posts = await getPorojects();
   return <HomePage posts={[]} />;
 }
