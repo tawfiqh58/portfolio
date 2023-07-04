3e82621d1ab86db87dcf3569ff04664d9c3108f9
Tawfiq Hasan <tawfiqhasan58@gmail.com>
2023-07-04 09:37:05 +0600

    fix: CORS issue

--- next.config.js
+++ next.config.js
@@ -16,6 +16,27 @@ const nextConfig = {
   eslint: {
     /// Set this to false if you want production builds to abort if there's lint errors
     ignoreDuringBuilds: process.env.VERCEL_ENV === "production"
+  },
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*"
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS"
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "X-Requested-With, Content-Type, Authorization"
+          }
+        ]
+      }
+    ];
   }
 };
 
